3fc0ca517d7adfd77d7055cab2a97405d4cf2403
TeddyT21 <111092662+TeddyT21@users.noreply.github.com>
2023-08-02 21:31:37 +0300

    Update 1-main.py

--- 0x00-personal_data/1-main.py
+++ 0x00-personal_data/1-main.py
@@ -8,7 +8,7 @@ import re
 
 RedactingFormatter = __import__('filtered_logger').RedactingFormatter
 
-message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+message = "name=Teddy;email=tmt2164@dylan.com;ssn=000-123-0000;password=Tedo;"
 log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
 formatter = RedactingFormatter(fields=("email", "ssn", "password"))
 print(formatter.format(log_record))
